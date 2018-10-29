057a02ff0d78f083b56391d7ab6e0c4dc4db0195
Abdi Sirat <abdikadar.sirat.2021@student.lhschools.org>
2018-10-29 13:57:00 -0400

    update

--- src/hangman.java
+++ src/hangman.java
@@ -1,5 +1,23 @@
 public class hangman {
+    private static String[] words = {"terminator", "banana", "computer", "cow", "rain", "water" };
+    private static String word = words[(int) (Math.random() * words.length)];
+    private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
+    private static int count = 0;
+
     public static void main(String[] args) {
-12112121
+
+    }
+
+    public static void hangmanImage() {
+        if (count == 1)
+            System.out.println("Wrong guess please try again.");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("----|----");
+        System.out.println("");
+
     }
 }
