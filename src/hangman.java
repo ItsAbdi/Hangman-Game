7773cfc98f570e582b6efd6736ab3363eb9092af
Abdi Sirat <abdikadar.sirat.2021@student.lhschools.org>
2018-11-01 13:58:34 -0400

    update

--- src/hangman.java
+++ src/hangman.java
@@ -1,23 +1,64 @@
 public class hangman {
-    private static String[] words = {"terminator", "banana", "computer", "cow", "rain", "water" };
-    private static String word = words[(int) (Math.random() * words.length)];
-    private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
-    private static int count = 0;
+
 
     public static void main(String[] args) {
 
     }
 
-    public static void hangmanImage() {
-        if (count == 1)
-            System.out.println("Wrong guess please try again.");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("----|----");
-        System.out.println("");
+    public static void hangmanImage(int tries) {
+       switch(tries){
+           case 0:
+               System.out.println("    |--------------------------|");
+               System.out.println("    |                        ");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("----|-----------------------------------");
+               System.out.println("");
+               break;
+           case 1:
+               System.out.println("    |--------------------------|");
+               System.out.println("    |                         (");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("    |");
+               System.out.println("----|-----------------------------------");
+               System.out.println("");
+
+               break;
+       }
+
+
 
     }
 }
