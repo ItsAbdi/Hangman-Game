229036211b3a21f22503a561f3093c2488a8b7d4
Abdi Sirat <abdikadar.sirat.2021@student.lhschools.org>
2018-11-01 14:17:36 -0400

    update

--- src/hangman.java
+++ src/hangman.java
@@ -6,57 +6,137 @@ public class hangman {
     }
 
     public static void hangmanImage(int tries) {
-       switch(tries){
-           case 0:
-               System.out.println("    |--------------------------|");
-               System.out.println("    |                        ");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("----|-----------------------------------");
-               System.out.println("");
-               break;
-           case 1:
-               System.out.println("    |--------------------------|");
-               System.out.println("    |                         (");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("    |");
-               System.out.println("----|-----------------------------------");
-               System.out.println("");
+        switch(tries){
+            case 0:
+                System.out.println("    |--------------------------|");
+                System.out.println("    |                        ");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("----|-----------------------------------");
+                System.out.println("");
+                break;
 
-               break;
-       }
+            case 1:
+                System.out.println("    |--------------------------|");
+                System.out.println("    |                         (");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("----|-----------------------------------");
+                System.out.println("");
+
+                break;
+
+            case 3:
+                System.out.println("    |--------------------------|");
+                System.out.println("    |                        ");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("----|-----------------------------------");
+                System.out.println("");
+
+                break;
+
+            case 4:
+                System.out.println("    |--------------------------|");
+                System.out.println("    |                        ");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("----|-----------------------------------");
+                System.out.println("");
+
+                break;
+
+
+            case 5:
+                System.out.println("    |--------------------------|");
+                System.out.println("    |                        ");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("----|-----------------------------------");
+                System.out.println("");
+
+                break;
+        }
 
 
 
